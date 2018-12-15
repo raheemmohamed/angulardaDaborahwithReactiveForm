c20461c87e952cb124aeedbe399b8ebf6b352e13
Raheem <mohamedraheem0555@gmail.com>
2018-12-15 20:52:48 +0530

    Angular reactive Form with Daborah init source

---
+++ Demo-start/src/app/customers/customer.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Customer } from './customer';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+  @Input() hero;
+
+  listFilter ;
+
+  customer = new Customer();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  save(customerForm: NgForm) {
+    console.log(customerForm.form);
+    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  }
+}
